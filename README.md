d114f8d18049ff1803a63df1c48cea41e32c4a1b
David Salgado <david@digitalronin.com>
2015-09-12 12:20:40 +0100

    expand a list of ranges supplied on stdin

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Iplist
 ======
 
 Given an IP range as a string (either CIDR or start..finish), list all IP numbers in the range.
+
+Usage:
+
+  cat foo | mix run -e Iplist.Lister.readlines
+
+Where foo contains lines such as;
+
+  1.2.3.4
+  1.2.3.0/24
+  1.2.3.4..1.2.3.5
+
+Output is a list of IPs, one per line
--- lib/iplist/lister.ex
+++ lib/iplist/lister.ex
@@ -1,5 +1,13 @@
 defmodule Iplist.Lister do
-  def list(str) do
-    [ str ]
+  def readlines do
+    case IO.read(:stdio, :line) do
+      :eof -> exit(:normal)
+      line -> expand(String.strip line)
+    end
+    readlines
+  end
+
+  defp expand(str) do
+    Iplist.Ip.range(str) |> Enum.map &Iplist.Ip.to_string(&1) |> IO.puts
   end
 end
