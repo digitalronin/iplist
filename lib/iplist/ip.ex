e6eed3c3c9d01766e639d669f0e41b1c81672599
David Salgado <david@digitalronin.com>
2015-09-12 13:11:15 +0100

    add doc strings and doc tests

--- lib/iplist/ip.ex
+++ lib/iplist/ip.ex
@@ -1,10 +1,42 @@
 defmodule Iplist.Ip do
+  @doc """
+  Given a tuple representing an IP number, return the next tuple.
+
+  ## Example
+
+    iex> Iplist.Ip.increment {1, 2, 3, 4}
+    {1, 2, 3, 5}
+    iex> Iplist.Ip.increment {1, 2, 254, 254}
+    {1, 3, 0, 0}
+
+  """
   def increment({254, 254, 254, 254}), do: raise "No more IPs!"
   def increment({a, 254, 254, 254}),   do: {a + 1, 0, 0, 0}
   def increment({a, b, 254, 254}),     do: {a, b + 1, 0, 0}
   def increment({a, b, c, 254}),       do: {a, b, c + 1, 0}
   def increment({a, b, c, d}),         do: {a, b, c, d + 1}
 
+  @doc """
+  Take an IP range in various formats and return a list of tuples.
+
+  ## Example
+
+    iex> Iplist.Ip.range "1.2.3.4"
+    [{1, 2, 3, 4}]
+
+    iex> Iplist.Ip.range "1.2.3.4..1.2.3.5"
+    [{1, 2, 3, 4}, {1, 2, 3, 5}]
+
+    iex> Iplist.Ip.range("1.2.3.4", "1.2.3.5")
+    [{1, 2, 3, 4}, {1, 2, 3, 5}]
+
+    iex> Iplist.Ip.range("1.2.3.4/31")
+    [{1, 2, 3, 4}, {1, 2, 3, 5}]
+
+    iex> Iplist.Ip.range({1, 2, 3, 4}, {1, 2, 3, 5})
+    [{1, 2, 3, 4}, {1, 2, 3, 5}]
+
+  """
   def range(str) when is_binary(str) do
     case String.split(str, "..") do
       [a, b] -> range(from_string(a), from_string(b))
@@ -23,11 +55,29 @@ defmodule Iplist.Ip do
     range(from_string(a), from_string(b))
   end
 
+  @doc """
+  Given a string representing an IP number, return the corresponding tuple.
+
+  ## Example
+
+    iex> Iplist.Ip.from_string "1.2.3.4"
+    {1, 2, 3, 4}
+
+  """
   def from_string(str) do
     list = String.split(str, ".") |> Enum.map &String.to_integer(&1)
     list_as_tuple list
   end
 
+  @doc """
+  Convert an IP number tuple to a string.
+
+  ## Example
+
+    iex> Iplist.Ip.to_string {1, 2, 3, 4}
+    "1.2.3.4"
+
+  """
   def to_string({a, b, c, d}), do: Enum.join([a, b, c, d], ".")
 
   defp range_from_cidr(ip, netmask) do
--- lib/iplist/lister.ex
+++ lib/iplist/lister.ex
@@ -1,4 +1,8 @@
 defmodule Iplist.Lister do
+  @doc """
+  Treat each line from standard input as a string representation of an IP range,
+  expand the range(s) and output each IP on a line.
+  """
   def readlines do
     case IO.read(:stdio, :line) do
       :eof -> exit(:normal)
@@ -7,6 +11,9 @@ defmodule Iplist.Lister do
     readlines
   end
 
+  @doc """
+  Expand a string representation of an IP range and output each IP on a line.
+  """
   def expand(str) do
     Iplist.Ip.range(str) |> Enum.map &Iplist.Ip.to_string(&1) |> IO.puts
   end
